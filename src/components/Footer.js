b2ec3275137474f3ce303e636971c846e2ea0a3d
Felicia <felicia.qf.chen@gmail.com>
2022-11-25 06:09:00 -0500

    :)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Footer.css";
 
 function Footer() {
@@ -8,9 +10,11 @@ function Footer() {
     <div className="footer">
       <div className="socialMedia">
         <InstagramIcon onClick={() => window.open('https://www.instagram.com/fei__felicia/')}/>
-        <LinkedInIcon onClick={() => window.open('https://www.linkedin.com/in/chen-felicia/')}/>
+        <LinkedInIcon onClick={() => window.open('https://www.linkedin.com/in/chen-felicia/')} />
+        <EmailIcon onClick={() => window.open('mailto:felicia.qf.chen@gmail.com')}/>
+        <GithubIcon onClick={() => window.open('https://github.com/fei-felicia-chen')}/>
       </div>
-      <p> &copy; Felicia Chen</p>
+      <p> Created by Felicia Chen &copy; 2022 </p>
     </div>
   );
 }
--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,12 +1,12 @@
-/* import Proj1 from "../assets/proj1.jpg";
-import Proj2 from "../assets/proj2.jpg";
+import Proj1 from "../assets/proj1.jpg";
+/*import Proj2 from "../assets/proj2.jpg";
 import Proj3 from "../assets/proj3.webp"; */
 
 export const ProjectList = [
   {
-    name: "Dijkstra Algorithm Visualizer",
-    //image: Proj1,
-    skills: "JavaScript,HTML,CSS",
+    name: "Blackjack Game",
+    image: Proj1,
+    skills: "C++",
   },
   {
     name: "Ecommerce Website",
