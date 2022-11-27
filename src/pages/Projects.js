f40fe9c8cf6c2b40d626be35e5a808505998776c
Felicia <felicia.qf.chen@gmail.com>
2022-11-26 23:49:10 -0500

    setup gh-pages

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,6 +8,7 @@ function Projects() {
   return (
     <div className="projects">
       <h1> Some of my projects...</h1>
+      <h2> Note: this section is in progress, see <a href="https://github.com/fei-felicia-chen">my GitHub</a></h2>
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return (
