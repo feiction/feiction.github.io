4ffc257f0ece4eb36b7339ba61c6a03526d5fb04
Felicia <felicia.qf.chen@gmail.com>
2022-11-27 00:15:54 -0500

    proj update

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
   return (
     <div className="projects">
       <h1> Some of my projects... AAAA</h1>
-      <h2> Note: this section is in progress, see <a href="https://github.com/fei-felicia-chen">my GitHub</a></h2>
+      <h2> Note: this section is in progress, to see my projects, go to <a href="https://github.com/fei-felicia-chen">my GitHub</a></h2>
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return (
