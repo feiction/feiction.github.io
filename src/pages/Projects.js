77bfa660b90ef4508235b5aec3aae4f24d202ac8
Felicia <felicia.qf.chen@gmail.com>
2022-11-28 19:14:51 -0500

    css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,8 +7,8 @@ import "../styles/Projects.css";
 function Projects() {
   return (
     <div className="projects">
-      <h1> Some of my projects... AAAA</h1>
-      <h2> Note: this section is in progress, to see my projects, go to <a href="https://github.com/fei-felicia-chen">my GitHub</a></h2>
+      <h1> Some of my projects...</h1>
+      <h2> Note: this section is in progress, to see my projects, visit <a href="https://github.com/fei-felicia-chen">my GitHub</a></h2>
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return (
